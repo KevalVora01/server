b9dd14ba09fbb618d4a0bb14e0482c91750ccd1c
Keval Vora <keval.vora@etatvasoft.com>
2026-07-30 09:19:17 +0530

    feat: refactor Sequelize configuration to use environment variables for database connection

--- src/shared/config/sequelize.ts
+++ src/shared/config/sequelize.ts
@@ -1,32 +1,32 @@
 import { Sequelize } from 'sequelize';
 import { env } from './env';
 
-// export const sequelize = new Sequelize({
-//   dialect: 'postgres',
-//   host: env.DATABASE_HOST,
-//   port: env.DATABASE_PORT,
-//   username: env.DATABASE_USER,
-//   password: env.DATABASE_PASSWORD,
-//   database: env.DATABASE_NAME,
-//   logging: env.NODE_ENV === 'development' ? console.log : false,
-//   define: {
-//     underscored: true,
-//     timestamps: true,
-//     freezeTableName: true,
-//   },
-//   pool: {
-//     max: 20,
-//     idle: 30000,
-//     acquire: 30000,
-//   }
-// });
-
-export const sequelize = new Sequelize(env.DATABASE_URL!, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+export const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: env.DATABASE_HOST,
+  port: env.DATABASE_PORT,
+  username: env.DATABASE_USER,
+  password: env.DATABASE_PASSWORD,
+  database: env.DATABASE_NAME,
+  logging: env.NODE_ENV === 'development' ? console.log : false,
+  define: {
+    underscored: true,
+    timestamps: true,
+    freezeTableName: true,
   },
+  pool: {
+    max: 20,
+    idle: 30000,
+    acquire: 30000,
+  }
 });
+
+// export const sequelize = new Sequelize(env.DATABASE_URL!, {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
